c9634feac4d57ff7364173133ee2af065187fcab
R. P. Dillon <rpdillon@etherplex.org>
2011-03-23 08:23:02 -0700

    Lots of little changes - new functions, changed paths, etc.  I think there are some bugs in the keybinding code that result in unecessary bindings, but everything's working at the moment.  Tested on Linux with Emacs 23.2 and Org 7.5.

--- init.el
+++ init.el
@@ -10,8 +10,26 @@
                          "org-mode" (expand-file-name
                                 "src" dotfiles-dir)))))
   (add-to-list 'load-path org-dir)
-  (require 'org-install)
+  (require 'org)
   (require 'ob))
 
 ; Load all literate org-mode files in this directory (any org-mode files residing there)
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(epa-popup-info-window nil)
+ '(erc-log-insert-log-on-open nil)
+ '(erc-modules (quote (autojoin button completion fill irccontrols list log match menu move-to-prompt netsplit networks noncommands readonly ring stamp track)))
+ '(htmlize-output-type (quote font))
+ '(org-confirm-elisp-link-function nil)
+ '(todo-file-do "~/Dropbox/memex/todo.txt")
+ '(todotxt-file "/home/rpdillon/Dropbox/memex/todo.txt" nil (todotxt)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
